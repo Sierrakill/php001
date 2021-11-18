04c9f406effc8a13cd68f6c64216040508784293
Sierrakill <92342394+Sierrakill@users.noreply.github.com>
2021-11-17 21:29:36 -0700

    Add files via upload

---
+++ EjerciciosBasicos/recibir.php
@@ -0,0 +1,24 @@
+<?php
+//echo "Bienvenido ". $_REQUEST["nombre"];
+
+/*if((int) $_REQUEST["nombre"]<18){
+    echo"No puede Conducir "." Su edad=".$_REQUEST["nombre"]." Es menor a la requerida";
+}*/
+/*if((int)$_REQUEST["nombre"]<3||(int)$_REQUEST["nombre"]==0){
+    echo"Calificacion Muy deficiente";
+}else if((int)$_REQUEST["nombre"]>=3 &&(int)$_REQUEST["nombre"]<5){
+    echo"Calificacion Insuficiente";
+}else if((int)$_REQUEST["nombre"]>=5&&(int)$_REQUEST["nombre"]<6){
+    echo"Calificacion Suficiente";
+}else if((int)$_REQUEST["nombre"]>=6 && (int)$_REQUEST["nombre"]<7){
+    echo"Calificacion Buena";
+}else if((int)$_REQUEST["nombre"]>=7 && (int)$_REQUEST["nombre"]<9){
+    echo"Calificacion Notable";
+}else if((int)$_REQUEST["nombre"]>=9 || (int)$_REQUEST["nombre"]==10){
+    echo"Calificacion Sobresaliente";
+}
+*/
+
+
+
+?>
